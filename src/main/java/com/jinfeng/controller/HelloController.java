9a3959cc164a1c32008e0a5e3c3fc5412982505f
yjfSystem <1786287141@qq.com>
2021-07-22 20:38:28 +0800

    开发人员添加了一行

--- src/main/java/com/jinfeng/controller/HelloController.java
+++ src/main/java/com/jinfeng/controller/HelloController.java
@@ -6,18 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.SQLOutput;
+
 @Controller
 public class HelloController {
 
     @ResponseBody
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello !";
     }
 
     @ResponseBody
     @RequestMapping("/hi")
-    public String hi(){
+    public String hi() {
+
+        System.out.println("hahahahh");
         return "hi";
     }
 }
